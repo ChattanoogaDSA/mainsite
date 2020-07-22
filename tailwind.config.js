30f6d49dae99457b478632a13d2e5246620017dd
Peter Sooley <psoots@gmail.com>
2020-07-21 20:31:47 -0400

    fix purged styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,20 @@ module.exports = {
   purge: {
     content: [
       './src/**/*.vue',
-      './src/**/*.js',
-      './src/**/*.jsx',
-      './src/**/*.html',
-      './src/**/*.pug',
-      './src/**/*.md',
+      './content/**/*.md',
     ],
-    whitelist: [
-      'body',
-      'html',
-      'img',
-      'a',
-      'g-image',
-      'g-image--lazy',
-      'g-image--loaded',
-      'svg-inline--fa',
-    ],
-    whitelistPatterns: [
-      /fa-$/,
-      /fc-$/,
-    ],
-    extractors: [
-      {
-        extractor: content => content.match(/[A-z0-9-:\\/]+/g),
-        extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
-      },
-    ],
+    options: {
+      whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+        'svg-inline--fa',
+      ],
+    },
   },
   theme: {
     fontFamily: {
