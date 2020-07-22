dea221f9a694f67fb13aa5b7df3890f96e1c12dd
Peter Sooley <psoots@gmail.com>
2020-07-22 07:30:12 -0400

    add CHA flower logo

--- src/components/LogoImage.vue
+++ src/components/LogoImage.vue
@@ -2,6 +2,6 @@
   <img class="w-16 h-16 md:w-32 md:h-32
               md:-ml-4 -ml-2
               self-center sm:self-auto"
-       src="../assets/images/logo-dsa-br-transparent.png"
+       src="../assets/images/cha-flower-450x450.svg"
        alt="Chattanooga DSA Banner Logo">
 </template>
