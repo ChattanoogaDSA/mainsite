1a601bf83d7d8c6747064a6768dd35f716fc4547
Peter Sooley <psoots@gmail.com>
2020-07-22 08:13:25 -0400

    add more footer style tweaks

--- src/components/FooterGroup.vue
+++ src/components/FooterGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col mr-0 sm:mr-4 mt-4 text-black-100 bg-white-100 border border-black-60 p-4">
+  <div class="flex flex-col mr-0 sm:mr-4 mt-4 text-white-100 bg-black-100 p-4">
     <h1 class="font-bold text-xl pb-2">
       <slot name="header"></slot>
     </h1>
--- src/components/FooterLink.vue
+++ src/components/FooterLink.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-1 flex">
-    <span class="bg-black-100 rounded-full w-2 h-2 self-center mr-2">&nbsp;</span>
+    <span class="bg-white-100 w-2 h-2 self-center mr-2">&nbsp;</span>
     <a class="no-underline hover:underline" v-bind="$attrs">
       <slot></slot>
     </a>
