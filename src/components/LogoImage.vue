794a86c735b1c2355a7fb72609af83537f1b2b08
Peter Sooley <psoots@gmail.com>
2020-07-28 07:38:41 -0400

    fix logo size on mobile, now bigger

--- src/components/LogoImage.vue
+++ src/components/LogoImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="w-16 h-16 md:w-32 md:h-32
+  <img class="w-24 h-24 md:w-32 md:h-32
               md:-ml-4 -ml-2
               self-center sm:self-auto"
        src="../assets/images/cha-flower-450x450.svg"
