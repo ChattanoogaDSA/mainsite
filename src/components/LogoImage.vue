55636aca9f907b964e6ea29fc7f711a03baf82be
Peter Sooley <psoots@gmail.com>
2020-09-29 13:32:47 -0400

    update logo image

--- src/components/LogoImage.vue
+++ src/components/LogoImage.vue
@@ -1,7 +1,7 @@
 <template>
-  <img class="w-24 h-24 md:w-32 md:h-32
-              md:-ml-4 -ml-2
+  <img class="w-24 h-24 -ml-2 p-2
+              md:w-32 md:h-32 md:-ml-4 p-2
               self-center sm:self-auto"
-       src="../assets/images/cha-flower-450x450.svg"
+       src="../assets/images/cha-dsa-flower.svg"
        alt="Chattanooga DSA Logo">
 </template>
